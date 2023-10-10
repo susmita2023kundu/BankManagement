d8bc34bb07e38259fa9dd9896c9ebd0cdac63090
susmita2023kundu <147513222+susmita2023kundu@users.noreply.github.com>
2023-10-10 19:22:43 +0530

    Add files via upload
    The system provides the access to the customer to create an 
    account, deposit/withdraw the cash from his account, also to view reports of all accounts present. 
    The customers can access the banks website for viewing their Account details and perform the 
    transactions on account as per their requirements. With Internet Banking, the brick and mortar 
    structure of the traditional banking gets converted into a click and portal model, thereby giving a 
    concept of virtual banking a real shape. Thus, today's banking is no longer confined to branches. 
    E-banking facilitates banking transactions by customers round the clock globally.

---
+++ Conn.java
@@ -0,0 +1,20 @@
+
+package bank.mangement.system;
+import java.sql.*;
+
+public class Conn {
+Connection c;
+Statement s;
+  
+    public Conn(){
+        try{
+           
+           c= DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem", "root" , "Root@8250");
+            s = c.createStatement();
+            
+        } catch (Exception e){
+            System.out.println(e);
+            
+        }
+    }
+}
